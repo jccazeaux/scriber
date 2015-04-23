c91ed26fcfc196fc1443291058994c4fdf3a48ce
Jean-Christophe Cazeaux <jccazeaux79@gmail.com>
2015-04-23 09:31:50 +0200

    Added text node

--- tests/scriberSpec.js
+++ tests/scriberSpec.js
@@ -12,4 +12,15 @@ describe("Constructs elements", function() {
 			.end().toString();
 		expect(elem).toBe('<div id="idTest" class="test-class"><span class="red"></span><input value="my value"></div>');
 	});
+
+	it("constructs child with text nodes", function() {
+		var elem = scriber.div().id("idTest").class("test-class")
+			.span().class("red").end()
+			.input().value("my value").end()
+			.text("Hello World !")
+			.br().end()
+			.end().toString();
+		expect(elem).toBe('<div id="idTest" class="test-class"><span class="red"></span><input value="my value">Hello World !<br></div>');
+	});
+
 });

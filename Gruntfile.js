6b0f78db6ff4573bf45c29fbefeba4976c090246
Jean-Christophe Cazeaux <jccazeaux1979@gmail.com>
2015-05-18 23:04:08 +0200

    First release

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,24 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+
+    pkg: grunt.file.readJSON('./package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+          '<%= grunt.template.today("yyyy-mm-dd") %>\n' + 
+          ' * https://github.com/jccazeaux/scriber\n' +
+          ' * Copyright (c) 2014 Jean-Christophe Cazeaux.\n' +
+          ' * Licensed under the MIT license.\n' +
+          ' */'
+      },
+      build: {
+        files: {
+          'dist/scriber.min.js': ['lib/scriber.js']
+        }
+      }
+    },
+
     jasmine: {
       tests: {
         src: ['lib/scriber.js'],
@@ -12,7 +30,7 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-jasmine');
-  
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
   grunt.registerTask('default', ['jasmine']);
-  
 };
